8d0e0feb19b5a3fd2f58513345d04ab31caf66dd
Brian David <bdavid1530@gmail.com>
2023-12-14 12:32:58 +0000

    Set up react-router-dom in App

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,9 +1,14 @@
-import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <div>portfolio</div>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ portfolio-website/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className={styles.homePage}>Home Page</h1>
+    </>
+  );
+};
+
+export default Home;
