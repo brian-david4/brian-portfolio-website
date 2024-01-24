98c33be0894acf93148dd5520590f2a6db416410
Brian David <bdavid1530@gmail.com>
2024-01-24 15:14:01 +0000

    HeaderNavLink hover animations

--- portfolio-website/src/components/HeaderNavMenu/NavBody/NavBody.tsx
+++ portfolio-website/src/components/HeaderNavMenu/NavBody/NavBody.tsx
@@ -1,16 +1,59 @@
 import { NavLink } from "../../../types";
 import styles from "./navBody.module.css";
-import HeaderNavLink from "../../HeaderNavLink/HeaderNavLink";
+import { ReactNode, useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { char_enter } from "../HeaderNavAnims";
+import { hoveredAnim } from "./navBodyAnim";
 
 interface NavBodyProps {
   links: NavLink[];
 }
 
 const NavBody = ({ links }: NavBodyProps) => {
+  const [hoveredLink, setHoveredLink] = useState({ isActive: false, index: 0 });
+
+  const getChars = (word: string) => {
+    let chars: ReactNode[] = [];
+    word.split("").forEach((char, i) => {
+      chars.push(
+        <motion.span
+          key={char + i}
+          custom={[i * 0.02, (word.length - i) * 0.01]}
+          variants={char_enter}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          {char}
+        </motion.span>
+      );
+    });
+    return chars;
+  };
+
   return (
     <div className={styles.body}>
       {links.map((link, idx) => (
-        <HeaderNavLink key={`l_${idx}`} title={link.title} to={link.to} />
+        <Link
+          onMouseEnter={() => setHoveredLink({ isActive: true, index: idx })}
+          onMouseLeave={() => setHoveredLink({ isActive: false, index: idx })}
+          className={styles.link}
+          key={`l_${idx}`}
+          to={link.to}
+        >
+          <motion.p
+            variants={hoveredAnim}
+            animate={
+              hoveredLink.isActive && hoveredLink.index != idx
+                ? "hovered"
+                : "inactive"
+            }
+            className={styles.title}
+          >
+            {getChars(link.title)}
+          </motion.p>
+        </Link>
       ))}
     </div>
   );
