ef069b69af437b039496c58ba644f9dc7bef8423
Brian David <bdavid1530@gmail.com>
2024-03-12 09:22:29 +0000

    Create responsive homepage title

--- portfolio-website/src/pages/Home/Home.tsx
+++ portfolio-website/src/pages/Home/Home.tsx
@@ -1,9 +1,15 @@
 import styles from "./home.module.css";
 
 const Home = () => {
+  const title =
+    "React developer with enthusiasm for design and creating simple yet beautiful websites.";
   return (
     <>
-      <div className={styles.homepage}></div>
+      <div className={styles.homepage}>
+        <div className={styles.titleWrapper}>
+          <h1 className={styles.title}>{title}</h1>
+        </div>
+      </div>
     </>
   );
 };
