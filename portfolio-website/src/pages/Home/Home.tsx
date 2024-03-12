d8fb5863910760c20dba70d9ed5817be154612b3
Brian David <bdavid1530@gmail.com>
2024-03-12 12:25:12 +0000

    homepage title entry animations

--- portfolio-website/src/pages/Home/Home.tsx
+++ portfolio-website/src/pages/Home/Home.tsx
@@ -1,13 +1,31 @@
+import { motion } from "framer-motion";
 import styles from "./home.module.css";
+import { titleWordsAnim } from "./homeAnims";
 
 const Home = () => {
   const title =
     "React developer with enthusiasm for design and creating simple yet beautiful websites.";
+  const titleWords = title.split(" ");
   return (
     <>
       <div className={styles.homepage}>
         <div className={styles.titleWrapper}>
-          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.title}>
+            {titleWords.map((word, idx) => (
+              <div className={styles.wordWrapper}>
+                <motion.div
+                  variants={titleWordsAnim}
+                  initial="initial"
+                  animate="enter"
+                  className={styles.word}
+                  key={idx}
+                  custom={idx}
+                >
+                  {word}
+                </motion.div>
+              </div>
+            ))}
+          </p>
         </div>
       </div>
     </>
---
+++ portfolio-website/src/pages/Home/homeAnims.ts
@@ -0,0 +1,14 @@
+export const titleWordsAnim = {
+  initial: {
+    y: "110%",
+    // opacity: 0,
+  },
+  enter: (n: number) => ({
+    y: "0%",
+    // opacity: 1,
+    transition: { duration: 1.8, delay: n * 0.05, ease: [0.76, 0, 0.24, 1] },
+  }),
+  exit: {
+    y: "-100%",
+  },
+};
