3a685a574b98f43a7fc2ade14afe3c3aea4896c9
Brian David <bdavid1530@gmail.com>
2024-03-14 17:41:59 +0000

    change nav menu easing

--- portfolio-website/src/components/HeaderNavMenu/navmenuAnim.ts
+++ portfolio-website/src/components/HeaderNavMenu/navmenuAnim.ts
@@ -1,16 +1,14 @@
-const easeInOutQuart = [0.76, 0, 0.24, 1];
-
 export const background = {
   initial: {
     top: "100vh",
   },
   animate: {
     top: "0vh",
-    transition: { duration: 1.4, ease: easeInOutQuart },
+    transition: { duration: 1.8, ease: [0.33, 1, 0.68, 1] },
   },
   exit: {
     top: "100vh",
-    transition: { duration: 0.6, ease: easeInOutQuart },
+    transition: { duration: 1, ease: [0.33, 1, 0.68, 1] },
   },
 };
 export const navMenu = {
@@ -19,10 +17,10 @@ export const navMenu = {
   },
   animate: {
     height: "auto",
-    transition: { duration: 0.8, ease: easeInOutQuart },
+    transition: { duration: 0.9, ease: [0.33, 1, 0.68, 1] },
   },
   exit: {
     height: "0",
-    transition: { duration: 1.2, ease: easeInOutQuart },
+    transition: { duration: 1.2, ease: [0.65, 0, 0.35, 1] },
   },
 };
