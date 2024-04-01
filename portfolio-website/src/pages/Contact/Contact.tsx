5da415c2b44eee4f3e220379165d0e8ea1687da1
Brian David <bdavid1530@gmail.com>
2024-04-01 11:44:11 +0100

    Contact text enter animations

--- portfolio-website/src/pages/Contact/Contact.tsx
+++ portfolio-website/src/pages/Contact/Contact.tsx
@@ -1,8 +1,13 @@
 import styles from "./contact.module.css";
 import ContactHello from "../../components/ContactHello/ContactHello";
 import ContactMouseEffect from "../../components/ContactMouseEffect/ContactMouseEffect";
+import { motion } from "framer-motion";
+import { wordEnter } from "./contactAnims";
 
 const Contact = () => {
+  const text1 = "I want to work with you!";
+  const text2 = "bdavid1530@gmail.com";
+
   return (
     <>
       <div className={styles.contactPage}>
@@ -10,15 +15,33 @@ const Contact = () => {
 
         <div className={styles.textFlex}>
           <div className={`${styles.text1Wrapper} ${styles.textWrapper}`}>
-            <p className={styles.textInner}>I want to work with you!</p>
+            <p className={styles.textInner}>
+              {text1.split(" ").map((ltr, idx) => (
+                <motion.span
+                  variants={wordEnter}
+                  custom={idx}
+                  initial="initial"
+                  animate="enter"
+                  key={`t_word_${idx}`}
+                  className={styles.word}
+                >
+                  {ltr}
+                </motion.span>
+              ))}
+            </p>
           </div>
           <div className={`${styles.text2Wrapper} ${styles.textWrapper}`}>
-            <a
+            <motion.a
               className={`${styles.textInner} ${styles.email}`}
               href="mailto:bdavid1530@gmail.com"
+              variants={wordEnter}
+              custom={0}
+              initial="initial"
+              animate="enter"
+              transition={{ delay: 0.75, duration: 1.4 }}
             >
-              bdavid1530@gmail.com
-            </a>
+              {text2}
+            </motion.a>
           </div>
         </div>
 
