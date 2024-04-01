989c0ef5aa736729546194e6ed67ae8dfe9c6a8d
Brian David <bdavid1530@gmail.com>
2024-04-01 11:17:25 +0100

    Refactor Contact Page

--- portfolio-website/src/pages/Contact/Contact.tsx
+++ portfolio-website/src/pages/Contact/Contact.tsx
@@ -1,54 +1,29 @@
-import { motion } from "framer-motion";
 import styles from "./contact.module.css";
-import { titleAnim } from "./contactAnims";
 import ContactHello from "../../components/ContactHello/ContactHello";
 import ContactMouseEffect from "../../components/ContactMouseEffect/ContactMouseEffect";
-import { useState } from "react";
-import ContactLink from "../../components/ContactLink/ContactLink";
 
 const Contact = () => {
-  const [isTitleHovered, setTitleHovered] = useState(false);
-  const title = "Contact";
-  const titleLetters = title.split("");
-  const emailTitle = "email";
-  const emailLink = "bdavid1530@gmail.com";
-
   return (
     <>
       <div className={styles.contactPage}>
-        <ContactMouseEffect isTitleHovered={isTitleHovered} />
+        <ContactMouseEffect isTitleHovered={true} />
+
+        <div className={styles.textFlex}>
+          <div className={`${styles.text1Wrapper} ${styles.textWrapper}`}>
+            <p className={styles.textInner}>I want to work with you!</p>
+          </div>
+          <div className={`${styles.text2Wrapper} ${styles.textWrapper}`}>
+            <a
+              className={`${styles.textInner} ${styles.email}`}
+              href="mailto:bdavid1530@gmail.com"
+            >
+              bdavid1530@gmail.com
+            </a>
+          </div>
+        </div>
 
         <div className={styles.phrasesPosition}>
           <ContactHello />
-        </div>
-        <div
-          onMouseEnter={() => setTitleHovered(true)}
-          onMouseLeave={() => setTitleHovered(false)}
-          className={styles.contactTitle}
-        >
-          {titleLetters.map((ltr, idx) => (
-            <div key={idx} className={styles.ltrWrapper}>
-              <motion.span
-                className={styles.letter}
-                variants={titleAnim}
-                initial="initial"
-                animate="enter"
-                custom={idx}
-              >
-                {ltr}
-              </motion.span>
-            </div>
-          ))}
-        </div>
-        <div className={styles.bio}>
-          <div className={styles.bioMessage}>
-            I want to hear from you, send me a message!
-          </div>
-          <div className={styles.bioHover}></div>
-        </div>
-        <div className={styles.linksWrapper}>
-          <ContactLink title={emailTitle} link={emailLink} />
-          <ContactLink title="phone" link="07838358167" />
         </div>
       </div>
     </>
