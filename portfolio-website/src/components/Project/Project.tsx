a6d6611e68352b4b0c53f5504cd94adb7e486fda
Brian David <bdavid1530@gmail.com>
2024-05-05 18:56:04 +0100

    Create desc for Project Component

--- portfolio-website/src/components/Project/Project.tsx
+++ portfolio-website/src/components/Project/Project.tsx
@@ -12,6 +12,13 @@ const Project = ({ project }: ProjectProps) => {
       <div className={styles.projectWrapper}>
         <div className={styles.title}>{project.title}</div>
 
+        <div className={styles.mainDesc}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          aliquid perspiciatis sed dolore! Sequi tempore, dicta eum similique
+          harum unde nesciunt, excepturi quaerat aliquid omnis explicabo quae
+          eaque commodi fugit?
+        </div>
+
         <Link to={project.link} className={styles.linkBtn}>
           <div className={styles.button}>
             <div className={styles.btnHeading}>Visit</div>
