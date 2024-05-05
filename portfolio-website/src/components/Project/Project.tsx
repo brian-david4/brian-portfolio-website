5d83f023f0b655b0280040e6757240a27c0f3f19
Brian David <bdavid1530@gmail.com>
2024-05-05 20:00:12 +0100

    Render images in Project Component

--- portfolio-website/src/components/Project/Project.tsx
+++ portfolio-website/src/components/Project/Project.tsx
@@ -7,6 +7,7 @@ interface ProjectProps {
 }
 
 const Project = ({ project }: ProjectProps) => {
+  const imageArr = project.images ? project.images : [""];
   return (
     <>
       <div className={styles.projectWrapper}>
@@ -20,13 +21,25 @@ const Project = ({ project }: ProjectProps) => {
         </div>
 
         <div className={styles.imagesSide}>
-          <div className={styles.img}></div>
-          <div className={styles.img}></div>
+          {imageArr.map(
+            (img, idx) =>
+              idx < 2 && (
+                <div className={styles.img} key={`img_${idx}`}>
+                  <img src={img} />
+                </div>
+              )
+          )}
         </div>
 
         <div className={styles.imagesBttm}>
-          <div className={styles.img}></div>
-          <div className={styles.img}></div>
+          {imageArr.map(
+            (img, idx) =>
+              idx > 1 && (
+                <div className={styles.img} key={`img_${idx}`}>
+                  <img src={img} />
+                </div>
+              )
+          )}
         </div>
 
         <Link target="_blank" to={project.link} className={styles.linkBtn}>
