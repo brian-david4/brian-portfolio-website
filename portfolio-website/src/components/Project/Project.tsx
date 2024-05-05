240bf5473f609b1db71e7deaad5300541359c444
Brian David <bdavid1530@gmail.com>
2024-05-05 20:57:46 +0100

    make project layout responsive

--- portfolio-website/src/components/Project/Project.tsx
+++ portfolio-website/src/components/Project/Project.tsx
@@ -20,26 +20,27 @@ const Project = ({ project }: ProjectProps) => {
           eaque commodi fugit?
         </div>
 
-        <div className={styles.imagesSide}>
-          {imageArr.map(
-            (img, idx) =>
-              idx < 2 && (
-                <div className={styles.img} key={`img_${idx}`}>
-                  <img src={img} />
-                </div>
-              )
-          )}
-        </div>
-
-        <div className={styles.imagesBttm}>
-          {imageArr.map(
-            (img, idx) =>
-              idx > 1 && (
-                <div className={styles.img} key={`img_${idx}`}>
-                  <img src={img} />
-                </div>
-              )
-          )}
+        <div className={styles.imagesBoth}>
+          <div className={styles.imagesSide}>
+            {imageArr.map(
+              (img, idx) =>
+                idx < 2 && (
+                  <div className={styles.img} key={`img_${idx}`}>
+                    <img src={img} />
+                  </div>
+                )
+            )}
+          </div>
+          <div className={styles.imagesBttm}>
+            {imageArr.map(
+              (img, idx) =>
+                idx > 1 && (
+                  <div className={styles.img} key={`img_${idx}`}>
+                    <img src={img} />
+                  </div>
+                )
+            )}
+          </div>
         </div>
 
         <Link target="_blank" to={project.link} className={styles.linkBtn}>
