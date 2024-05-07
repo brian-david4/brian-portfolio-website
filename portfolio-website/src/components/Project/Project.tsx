efb26972e8d8e22f3993d0b059f7810a1471d418
Brian David <bdavid1530@gmail.com>
2024-05-07 13:56:03 +0100

    Project InView color animations

--- portfolio-website/src/components/Project/Project.tsx
+++ portfolio-website/src/components/Project/Project.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+
+import Carousel from "../Carousel/Carousel";
+
 import { ProjectType } from "../../types";
 import styles from "./proj.module.css";
-import Carousel from "../Carousel/Carousel";
+import { motion } from "framer-motion";
 
 interface ProjectProps {
   project: ProjectType;
 }
 
 const Project = ({ project }: ProjectProps) => {
+  const [projectRef, inView] = useInView({
+    threshold: 0.6,
+    triggerOnce: false,
+  });
   const imageArr = project.images ? project.images : [""];
   return (
     <>
-      <div className={styles.projectWrapper}>
+      <motion.div
+        animate={{ backgroundColor: inView ? "#ebe4d8" : "#00000" }}
+        transition={{ duration: 1.2, ease: [0.5, 1, 0.89, 1] }}
+        ref={projectRef}
+        className={styles.projectWrapper}
+      >
         <Carousel items={[project.title, project.title, project.title]} />
 
         <div className={styles.mainDesc}>{project.desc}</div>
@@ -45,7 +58,7 @@ const Project = ({ project }: ProjectProps) => {
             <div className={styles.hoverFill} />
           </div>
         </Link>
-      </div>
+      </motion.div>
     </>
   );
 };
